ded192c983acd86cf61c57e95f912bae97761cbc
pedropacheco95 <pedropacheco95@gmail.com>
2022-06-09 01:55:59 +0100

    Removal of unused code

--- teresaefrancisco/modules/upload.py
+++ teresaefrancisco/modules/upload.py
@@ -1,4 +1,3 @@
-import pandas
 import os
 import csv
 
@@ -7,60 +6,6 @@ from flask import Blueprint, redirect, url_for
 bp = Blueprint('upload', __name__, url_prefix='/upload')
 
 from teresaefrancisco.models import Product , ProductImage , Contribution
-
-@bp.route("/from_csv", methods =["GET", "POST"])
-def from_csv():
-    
-    products_table = pandas.read_csv('/Users/pedropacheco/Documents/Projetos/teresaefrancisco/teresaefrancisco/teresaefrancisco/static/data/csv/products.csv')
-    contributions_table = pandas.read_csv('/Users/pedropacheco/Documents/Projetos/teresaefrancisco/teresaefrancisco/teresaefrancisco/static/data/csv/contribution.csv')
-    product_images_table = pandas.read_csv('/Users/pedropacheco/Documents/Projetos/teresaefrancisco/teresaefrancisco/teresaefrancisco/static/data/csv/product_images.csv')
-
-    for index, row in products_table.iterrows():
-        if not pandas.isna(row['name']):
-            
-            name = row['name']
-            description = row['description']
-            price = float(row['price'])
-            price_paid = float(row['price_paid'])
-            store = row['store']
-            for_honeymoon = bool(row['for_honeymoon'])
-
-            product = Product(name=name,price=price)
-            if description:
-                product.store = description
-            if price_paid:
-                product.price_paid = price_paid
-            if store:
-                product.store = store
-            if for_honeymoon:
-                product.for_honeymoon = for_honeymoon
-            product.create()
-    
-    for index, row in contributions_table.iterrows():
-        if not pandas.isna(row['name']):
-            
-            name = row['name']
-            value_contributed = float(row['value_contributed'])
-            message = row['message']
-            product_id = int(row['product_id'])
-
-            contribution = Contribution(name=name,value_contributed=value_contributed,product_id=product_id)
-            if message:
-                contribution.message = message
-            contribution.create()
-
-    for index, row in product_images_table.iterrows():
-        if not pandas.isna(row['path']):
-            
-            path = row['path']
-            product_id = int(row['product_id'])
-            new_image = ProductImage(path=path)
-
-            product = Product.query.filter_by(id=product_id).first()
-            new_image.product = product
-            new_image.create()
-
-    return redirect(url_for('main.index'))
 
 @bp.route("/to_csv", methods =["GET", "POST"])
 def to_csv():
@@ -90,32 +35,4 @@ def to_csv():
             
             write.writerow(fields)
             write.writerows(rows)
-    return redirect(url_for('main.index'))
-
-@bp.route("/from_csv_made_from_excel", methods =["GET", "POST"])
-def from_csv_made_from_excel():
-    
-    products_table = pandas.read_csv('/Users/pedropacheco/Documents/Projetos/teresaefrancisco/teresaefrancisco/teresaefrancisco/static/data/products.csv')
-
-    for index, row in products_table.iterrows():
-        if not pandas.isna(row['Name']):
-            
-            name = row['Name']
-            description = row['Description']
-            price = float(row['Price'])
-            store = row['Store']
-
-            product = Product(name=name,price=price)
-            if description:
-                product.store = description
-            if store:
-                product.store = store
-            product.create()
-
-            images = row['URL'].split(';')
-            for image in images:
-                new_image = ProductImage(path=image,product_id=product.id)
-                new_image.create()
-
-
     return redirect(url_for('main.index'))

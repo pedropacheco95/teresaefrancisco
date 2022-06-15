6986976dbaf69710fe26881fe5cd59ddcca5dda0
pedropacheco95 <pedropacheco95@gmail.com>
2022-06-15 03:30:06 +0100

    Corrections

--- teresaefrancisco/models/specific_info.py
+++ teresaefrancisco/models/specific_info.py
@@ -3,7 +3,7 @@ from teresaefrancisco.sql_db import db
 from sqlalchemy import Column, Integer , Text , JSON
 
 class SpecificInfo(db.Model ,model.Model,model.Base):
-    __tablename__ = 'confirmation'
+    __tablename__ = 'specific_info'
     __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
     name = Column(Text)
